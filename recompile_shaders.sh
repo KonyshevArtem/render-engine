32c3374aa24f4f92284157c255ff347ffeca04e5
artemkonyshev <konyshev.ar@gmail.com>
2024-10-08 00:20:09 +0300

    Implement Final Blit pass in HLSL
    - Add recompile_shaders script

---
+++ recompile_shaders.sh
@@ -0,0 +1,8 @@
+./cmake-build-debug/shader_compiler/ShaderCompiler opengl resources/shaders/final_blit/final_blit.hlsl
+./cmake-build-debug/shader_compiler/ShaderCompiler metal resources/shaders/final_blit/final_blit.hlsl
+
+./cmake-build-debug/shader_compiler/ShaderCompiler opengl resources/shaders/fallback/fallback.hlsl
+./cmake-build-debug/shader_compiler/ShaderCompiler metal resources/shaders/fallback/fallback.hlsl
+
+./cmake-build-debug/shader_compiler/ShaderCompiler opengl resources/shaders/skybox/skybox.hlsl
+./cmake-build-debug/shader_compiler/ShaderCompiler metal resources/shaders/skybox/skybox.hlsl

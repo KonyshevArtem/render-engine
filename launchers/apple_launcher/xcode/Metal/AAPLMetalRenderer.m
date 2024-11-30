3e17a3b60357328f5cc318747245d104845673c9
artemkonyshev <konyshev.ar@gmail.com>
2024-11-30 15:38:17 +0200

    Add separate command queue for Copy operations
    - Create copy command buffer from Copy queue. This allows running render and copy command in parallel
    - Cleanup Metal Renderer class

--- launchers/apple_launcher/xcode/Metal/AAPLMetalRenderer.m
+++ launchers/apple_launcher/xcode/Metal/AAPLMetalRenderer.m
@@ -8,11 +8,11 @@ static const MTLPixelFormat AAPLColorFormat = MTLPixelFormatBGRA8Unorm;
 /// Main class that performs the rendering.
 @implementation AAPLMetalRenderer
 
-id<MTLDevice> _device;
-id<MTLCommandQueue> _commandQueue;
-id<MTLDepthStencilState> _depthState;
+id<MTLDevice> s_Device;
+id<MTLCommandQueue> s_RenderCommandQueue;
+id<MTLCommandQueue> s_CopyCommandQueue;
 
-CGSize _viewSize;
+CGSize s_ViewSize;
 
 /// Initialize the renderer with the MetalKit view that references the Metal device you render with.
 /// You also use the MetalKit view to set the pixel format and other properties of the drawable.
@@ -21,29 +21,25 @@ CGSize _viewSize;
     self = [super init];
     if(self)
     {
-        _device = mtkView.device;
-        _commandQueue = [_device newCommandQueue];
-
-        mtkView.colorPixelFormat        = AAPLColorFormat;
-        mtkView.depthStencilPixelFormat = AAPLDepthFormat;
-        {
-            // Configure a combined depth and stencil descriptor that enables the creation
-            // of an immutable depth and stencil state object.
-            MTLDepthStencilDescriptor *depthStencilDesc = [[MTLDepthStencilDescriptor alloc] init];
-            depthStencilDesc.depthCompareFunction = MTLCompareFunctionLess;
-            depthStencilDesc.depthWriteEnabled = YES;
-            _depthState = [_device newDepthStencilStateWithDescriptor:depthStencilDesc];
-        }
+        s_Device = mtkView.device;
+        s_RenderCommandQueue = [s_Device newCommandQueue];
+        s_CopyCommandQueue = [s_Device newCommandQueue];
         
-        id<MTLCommandBuffer> renderCommandBuffer = [_commandQueue commandBuffer];
-        id<MTLCommandBuffer> copyCommandBuffer = [_commandQueue commandBuffer];
+        [s_RenderCommandQueue setLabel:@"Render Queue"];
+        [s_CopyCommandQueue setLabel:@"Copy Queue"];
+
+        mtkView.colorPixelFormat = AAPLColorFormat;
+        mtkView.depthStencilPixelFormat = AAPLDepthFormat;
+        
+        id<MTLCommandBuffer> renderCommandBuffer = [s_RenderCommandQueue commandBuffer];
+        id<MTLCommandBuffer> copyCommandBuffer = [s_CopyCommandQueue commandBuffer];
         
         NSString* executablePath = [[NSBundle mainBundle] executablePath];
         NSString* resourcePath = [[NSBundle mainBundle] resourcePath];
         
-        [EngineFrameworkWrapper Initialize:_device renderCommandBuffer:renderCommandBuffer copyCommandBuffer:copyCommandBuffer executablePath:[executablePath UTF8String] resourcesPath:[resourcePath UTF8String]];
+        [EngineFrameworkWrapper Initialize:s_Device renderCommandBuffer:renderCommandBuffer copyCommandBuffer:copyCommandBuffer executablePath:[executablePath UTF8String] resourcesPath:[resourcePath UTF8String]];
         [ImGuiWrapper Init_OSX:mtkView];
-        [ImGuiWrapper Init_Metal:_device];
+        [ImGuiWrapper Init_Metal:s_Device];
         
         [copyCommandBuffer commit];
         [renderCommandBuffer commit];
@@ -62,7 +58,7 @@ CGSize _viewSize;
 /// Called whenever the view orientation, layout, or size changes.
 - (void) mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size
 {
-    _viewSize = size;
+    s_ViewSize = size;
 }
 
 /// Called whenever the view needs to render.
@@ -77,13 +73,13 @@ CGSize _viewSize;
 #endif
     
     MTLRenderPassDescriptor* renderPassDescriptor = [view currentRenderPassDescriptor];
-    id<MTLCommandBuffer> renderCommandBuffer = [_commandQueue commandBuffer];
-    id<MTLCommandBuffer> copyCommandBuffer = [_commandQueue commandBuffer];
+    id<MTLCommandBuffer> renderCommandBuffer = [s_RenderCommandQueue commandBuffer];
+    id<MTLCommandBuffer> copyCommandBuffer = [s_CopyCommandQueue commandBuffer];
     
     [ImGuiWrapper NewFrame_Metal:renderPassDescriptor];
     [ImGuiWrapper NewFrame_OSX:view];
     
-    [EngineFrameworkWrapper TickMainLoop:renderCommandBuffer copyCommandBuffer:copyCommandBuffer backbufferDescriptor:renderPassDescriptor width:_viewSize.width height:_viewSize.height];
+    [EngineFrameworkWrapper TickMainLoop:renderCommandBuffer copyCommandBuffer:copyCommandBuffer backbufferDescriptor:renderPassDescriptor width:s_ViewSize.width height:s_ViewSize.height];
     
     [[[renderPassDescriptor colorAttachments] objectAtIndexedSubscript:0] setLoadAction:MTLLoadActionLoad];
     id<MTLRenderCommandEncoder> commandEncoder = [renderCommandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];

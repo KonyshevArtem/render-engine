5796c972289605e4d442f8b4e645396a951db4fe
artem.konyshev <konyshev.ar@gmail.com>
2024-12-30 16:58:32 +0200

    Fix some appearance issues
    - Lock activity to landscape orientation
    - Enable immersive fullscreen mode

--- launchers/android_launcher/AndroidStudio/app/src/main/java/com/artemkonyshev/renderengine/MainActivity.java
+++ launchers/android_launcher/AndroidStudio/app/src/main/java/com/artemkonyshev/renderengine/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 public class MainActivity extends Activity
 {
     private GLSurfaceView m_GlSurfaceView;
@@ -12,6 +16,10 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        insetsController.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        insetsController.hide(WindowInsetsCompat.Type.systemBars());
 
         m_GlSurfaceView = new RenderEngineGLSurfaceView(this);
         setContentView(m_GlSurfaceView);

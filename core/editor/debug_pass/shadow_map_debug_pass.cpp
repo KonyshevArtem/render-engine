4a641f3ccd67ffa25f688eb1287546b38b1bd244
ArtemKonyshev <konyshev.ar@gmail.com>
2025-06-15 17:45:02 +0300

    Improve shadow debug pass
    - Add parameters for visualization and light selection

--- core/editor/debug_pass/shadow_map_debug_pass.cpp
+++ core/editor/debug_pass/shadow_map_debug_pass.cpp
@@ -4,6 +4,8 @@
 #include "shader/shader.h"
 #include "material/material.h"
 #include "graphics_backend_api.h"
+#include "graphics_buffer/graphics_buffer.h"
+#include "types/graphics_backend_buffer_info.h"
 
 const bool s_Enabled = false;
 
@@ -14,12 +16,38 @@ ShadowMapDebugPass::ShadowMapDebugPass(int priority) : RenderPass(priority)
 
 void ShadowMapDebugPass::Execute(const Context& ctx)
 {
+    struct DebugData
+    {
+        uint32_t LightType;
+        uint32_t LightIndex;
+        uint32_t PointLightSide;
+        float ScreenAspect;
+
+        float Scale;
+        float MinDepth;
+        float MaxDepth;
+        float Padding0;
+    };
+
     static std::shared_ptr<Shader> shader = Shader::Load("core_resources/shaders/editor/shadowMapDebug", {}, {}, {CullFace::NONE, CullFaceOrientation::CLOCKWISE}, {false, DepthFunction::ALWAYS});
     static std::shared_ptr<Material> material = std::make_shared<Material>(shader, "ShadowMap Debug");
+    static std::shared_ptr<GraphicsBuffer> buffer = std::make_shared<GraphicsBuffer>(sizeof(DebugData), "ShadowMapDebugData");
+    static GraphicsBackendResourceBindings bindings = shader->GetBuffers().at("DebugData")->GetBinding();
 
     if (s_Enabled)
     {
+        DebugData data{};
+        data.LightType = 0;
+        data.LightIndex = 0;
+        data.PointLightSide = 0;
+        data.ScreenAspect = static_cast<float>(Graphics::GetScreenWidth()) / static_cast<float>(Graphics::GetScreenHeight());
+        data.Scale = 0.4f;
+        data.MinDepth = 0.0f;
+        data.MaxDepth = 1.0f;
+
         GraphicsBackend::Current()->BeginRenderPass("ShadowMap Debug Pass");
+        buffer->SetData(&data, 0, sizeof(data));
+        GraphicsBackend::Current()->BindConstantBuffer(buffer->GetBackendBuffer(), bindings, 0, sizeof(data));
         Graphics::Draw(*m_FullscreenMesh, *material, Matrix4x4::Identity());
         GraphicsBackend::Current()->EndRenderPass();
     }
--- core_resources/shaders/editor/shadowMapDebug.hlsl
+++ core_resources/shaders/editor/shadowMapDebug.hlsl
@@ -13,22 +13,53 @@ struct Varyings
     float2 UV               : TEXCOORD2;
 };
 
+cbuffer DebugData : register(b2)
+{
+    uint _LightType;
+    uint _LightIndex;
+    uint _PointLightSide;
+    float _ScreenAspect;
+
+    float _Scale;
+    float _MinDepth;
+    float _MaxDepth;
+    float Padding0;
+}
+
 Varyings vertexMain(Attributes attributes)
 {
     Varyings vars;
 
-    vars.PositionCS = float4((attributes.vertPositionOS.xy + 1.0) * 0.3 - 1.0, 1, 1);
+    float2 xy = (attributes.vertPositionOS.xy + 1.0) * _Scale;
+    xy.x /= _ScreenAspect;
+    xy -= 1.0;
+
+    vars.PositionCS = float4(xy, 1, 1);
     vars.UV = attributes.texCoord;
 
     return vars;
 }
 
-half4 fragmentMain(Varyings vars) : SV_Target
+
+float4 fragmentMain(Varyings vars) : SV_Target
 {
+    const uint DIRECTIONAL = 0;
+    const uint POINT = 1;
+    const uint SPOT = 2;
+
 #if SCREEN_UV_UPSIDE_DOWN
     vars.UV.y = 1 - vars.UV.y;
 #endif
 
-    half4 depth = _DirLightShadowMap.Sample(sampler_DirLightShadowMap, vars.UV);
-    return half4(depth.x, 0, 0, 1.0);
+    float depth = 0;
+    if (_LightType == DIRECTIONAL)
+        depth = _DirLightShadowMap.Sample(sampler_DirLightShadowMap, vars.UV).x;
+    else if (_LightType == POINT)
+        depth = _PointLightShadowMapArray.Sample(sampler_PointLightShadowMapArray, float3(vars.UV, _LightIndex * 6 + _PointLightSide)).x;
+    else if (_LightType == SPOT)
+        depth = _SpotLightShadowMapArray.Sample(sampler_SpotLightShadowMapArray, float3(vars.UV, _LightIndex)).x;
+
+    depth = (depth - _MinDepth) / (_MaxDepth - _MinDepth);
+
+    return float4(depth, 0, 0, 1.0);
 }

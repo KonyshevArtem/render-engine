16328c6885ee18fa414f0c3dc315a264e8273916
artemkonyshev <konyshev.ar@gmail.com>
2025-07-09 02:03:53 +0300

    Fix MacOS retina
    - Update drawable size with a scale factor, and pass updated size into engine

--- launchers/apple_launcher/xcode/Metal/AAPLMetalViewController.m
+++ launchers/apple_launcher/xcode/Metal/AAPLMetalViewController.m
@@ -23,13 +23,7 @@ AAPLMetalRenderer *_renderer;
     _view = (AAPLMetalView *)self.view;
     _view.device = MTLCreateSystemDefaultDevice();
 
-#if !defined(TARGET_IOS) && !defined(TARGET_TVOS)
-    [_view setFrameSize:NSSizeFromCGSize(CGSizeMake(1920, 1080))];
-#endif
-
     _renderer = [[AAPLMetalRenderer alloc] initWithMetalKitView:_view];
-    [_renderer mtkView:_view drawableSizeWillChange:_view.drawableSize];
-
     _view.delegate = _renderer;
 }
 

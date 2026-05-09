4ee753589431fefcd925b67a03a0b7eb32c6ac72
Artem Konyshev <konyshev.ar@gmail.com>
2026-05-10 00:16:35 +0300

    Add templated Get function for Arguments

--- utils/arguments.h
+++ utils/arguments.h
@@ -1,6 +1,7 @@
 #ifndef RENDER_ENGINE_ARGUMENTS_H
 #define RENDER_ENGINE_ARGUMENTS_H
 
+#include <charconv>
 #include <string>
 
 namespace Arguments
@@ -8,6 +9,21 @@ namespace Arguments
     void Init(char** argv, int argc);
     bool Contains(const std::string& argument);
     std::string Get(const std::string& argument);
+
+    template<typename T>
+    T Get(const std::string& argument)
+	{
+        const std::string strVal = Get(argument);
+        if (strVal.empty())
+            return T();
+
+        T value;
+        auto [ptr, ec] = std::from_chars(strVal.data(), strVal.data() + strVal.size(), value);
+        if (ec != std::errc{})
+	        return T();
+
+        return value;
+    }
 }
 
 #endif //RENDER_ENGINE_ARGUMENTS_H
